e25d8499e58fc45ad510d1ac2373a0a8f48e0139
daisuke sonoda <dksonoda@gmail.com>
2024-01-05 21:37:09 +0900

    edit

--- environments/development/outputs.tf
+++ environments/development/outputs.tf
@@ -19,3 +19,19 @@ output "private_subnet_ids" {
 output "private_db_subnet_ids" {
   value = module.vpc.private_db_subnet_ids
 }
+
+output "igw_id" {
+  value = module.vpc.igw_id
+}
+
+output "nat_eips" {
+  value = module.vpc.nat_eips
+}
+
+output "nat_eips_public_ips" {
+  value = module.vpc.nat_eips_public_ips
+}
+
+output "natgw_ids" {
+  value = module.vpc.natgw_ids
+}
